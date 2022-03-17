e385b11188a1cf0e7c829e2cad98fff42048fdcf
Matthew Young <may0004@uah.edu>
2022-03-16 19:18:24 -0500

    Got two SOA services created. Need to refactor to make configuration more generic

---
+++ WhitespaceApplication/WhitespaceCounterService/Controllers/WhitespaceCounterService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace WhitespaceCounterService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WhitespaceCounterService : ControllerBase
+    {
+        private readonly ILogger<WhitespaceCounterService> _logger;
+        public WhitespaceCounterService(ILogger<WhitespaceCounterService> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{inputString}")]
+        public int Get(string inputString)
+        {
+            _logger.LogInformation($"Received request to count whitespace for {inputString}");
+            _logger.LogInformation($"Counted {inputString.Count(Char.IsWhiteSpace)} whitespace characters");
+            return inputString.Count(Char.IsWhiteSpace);
+        }
+    }
+}
---
+++ WhitespaceApplication/WhitespaceRemovalService/Controllers/WhitespaceRemovalService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WhitespaceRemovalService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WhitespaceRemovalService : ControllerBase
+    {
+        private readonly ILogger<WhitespaceRemovalService> _logger;
+        public WhitespaceRemovalService(ILogger<WhitespaceRemovalService> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{inputString}")]
+        public string Get(string inputString)
+        {
+            _logger.LogInformation($"Received request to trim whitespace for {inputString}");
+            _logger.LogInformation($"Trimmed whitespace string is {inputString.Replace(" ", string.Empty)}");
+            return inputString.Replace(" ", string.Empty);
+        }
+    }
+}
