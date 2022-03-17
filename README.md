5acd364495fa7619c33549e42b796836582094bc
Matthew Young <may0004@uah.edu>
2022-03-16 20:21:58 -0500

    Added API Call on button click to web page but need to allow CORS in the WebAPI servers

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 2. `whitespace-removal-service` - responsible for receiving strings with whitespace and returning a response with whitespace removed
 3. `whitespace-counter-service` - responsible for receiving strings with whitespace and returning a response with the number of whitespace characters in the string 
 
+# Some Example API Calls
+- `httprepl http://localhost:5000`
+- `curl http://localhost:5000/WhitespaceRemovalService/{some%20String} -v`
+
 # References
 - [Microsoft ASP.Net Web API Getting Started Guide](https://docs.microsoft.com/en-us/learn/modules/build-web-api-aspnet-core/3-exercise-create-web-api)
--- webui/js/main.js
+++ webui/js/main.js
@@ -1,5 +1,21 @@
+let WHITESPACE_REMOVAL_SERVICE_URL = 'http://localhost:5000/WhitespaceRemovalService/'
+let WHITESPACE_COUNTER_SERVICE_URL = 'http://localhost:5000/WhitespaceCounterService/'
+
 function GetWhitespaceResult() {
     var inputString = document.getElementById("inputString").value;
+    httpGetAsync(WHITESPACE_REMOVAL_SERVICE_URL + inputString, function(result){
+        console.log(result);
+    });
     document.getElementById("stripped-whitespace-result").innerHTML = "Stripped Whitespace: " + inputString;
     document.getElementById("whitespace-count-result").innerHTML = "Number of Whitespace Characters: 3";
 }
+
+function httpGetAsync(theUrl, callback) {
+    let xmlHttpReq = new XMLHttpRequest();
+    xmlHttpReq.onreadystatechange = function () {
+      if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200)
+        callback(xmlHttpReq.responseText);
+    }
+    xmlHttpReq.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttpReq.send(null);
+  }
