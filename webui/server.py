d585e690bc97070d9c39daa9eb527a4df90a579d
Matthew Young <may0004@uah.edu>
2022-03-23 17:36:54 -0500

    Updated python web server

--- webui/server.py
+++ webui/server.py
@@ -6,9 +6,7 @@ IP = "127.0.0.1"
 PORT = 8000
 URL = "http://" + IP + ":" + str(PORT)
 
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer((IP, PORT), Handler) as httpd:
+with socketserver.TCPServer((IP, PORT), http.server.SimpleHTTPRequestHandler) as httpd:
     print(f"serving on {URL}")
     webbrowser.open(URL)
     httpd.serve_forever()
